c52db288566c1d1901880894992f2915e1839fd3
AagastyaN9 <agastya.naik27@gmail.com>
2024-12-02 15:06:46 +0400

    Add files via upload

---
+++ dict.lua
@@ -0,0 +1,7 @@
+local food = "orange"
+print(#food)
+
+local foods = {"orange", "bacon", "steak", "pizza"}
+
+local foods_type = type(foods)
+print(foods[2])
---
+++ operations.lua
@@ -0,0 +1,32 @@
+--operations--
+
+local result1 = 3+2
+print(result1)
+
+local result2 = 5-5
+print(result2)
+
+local result3 = 5/10
+print(result3)
+
+local result4 = 5 * 4
+print(result4)
+
+local result5 = 5^4
+print(result5)
+
+--there are many more operations, but for now there are only 5--
+
+--math--
+
+local result = -4 * 3
+local result_abs = math.abs(result)
+print(result)
+print(result_abs)
+
+local result1.1 = 5/3
+local result_ceil = math.ceil(result)
+print(result1.1)
+print(result_ceil)
+
+--there are much more, that you can try out. check the lua documentation--
