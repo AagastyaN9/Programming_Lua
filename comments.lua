b7be42f29dcba0fee88ec9092cc65187a1713f6d
AagastyaN9 <agastya.naik27@gmail.com>
2024-12-03 12:30:07 +0400

    Add files via upload

---
+++ comments.lua
@@ -0,0 +1,12 @@
+--This is a comment--
+
+--we have already discussed this in main.lua--
+--this is just another recap--
+
+--first you put two dashes, your comment and close with another two dashes--
+
+--[[
+    this is
+    a comment
+    of more lines.  --you do this by putting the dashes then add two long brackets and then type your comment--
+]]
---
+++ local vs Global variables.lua
@@ -0,0 +1,12 @@
+message5 = 10
+message7 = 20
+
+-- this variables are called global variables
+
+local m = 2
+local e = 6
+
+--this is a local variable
+
+--[[global variables are used through the whole project 
+while loca variables are focused on one section of code!]]
